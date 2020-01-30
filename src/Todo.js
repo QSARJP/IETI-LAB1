7a7f93fa044e6455db43150cc122fe119a158f6c
Juan Ospina <juan.ospina-h@mail.escuelaing.edu.co>
2020-01-30 11:46:54 -0500

    Part I and Part II

---
+++ src/Todo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export class Todo extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            //Add your code here to represent a TODO
+            <div>
+                <h1>Text: {this.props.text}</h1>
+                <h1>Priority: {this.props.priority}</h1>
+                <h1>Due Date: {this.props.dueDate.toString()}</h1>
+            </div>
+
+        );
+    }
+
+}
